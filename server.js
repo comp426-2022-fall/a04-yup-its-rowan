99b1e8c7a8790d65df5055fb2a46560c406fd8f8
Rohan Akki <roha.akki@gmail.com>
2022-12-01 19:06:31 -0500

    fixes capitalization erorr

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import {roll} from "./lib/roll.js";
 import minimist from "minimist";
-import { Express } from "express";
+import { express } from "express";
 
 let args = minimist(process.argv.slice(2));
-let expressApp = Express();
+let expressApp = express();
 
 var port = args.port || 5000; //either the port or 5000
 
-expressApp.use(Express.urlencoded({ extended: true })); //
+expressApp.use(Express.urlencoded({ extended: true })); //extending to url encoded or json doesn't matter and then listen
 expressApp.listen(port);
+
+expressApp.get("/app", (req, res) => { //get request
+  res.status(200).send("200 OK"); //send 200 OK for the first thing
+});
 
