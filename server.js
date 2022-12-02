07ce012b0ff10bb3b4dc8d7fdefb2b27971a23de
Rohan Akki <roha.akki@gmail.com>
2022-12-01 19:03:27 -0500

    testing autograder responses

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ import {roll} from "./lib/roll.js";
 import minimist from "minimist";
 import { Express } from "express";
 
+let args = minimist(process.argv.slice(2));
+let expressApp = Express();
+
+var port = args.port || 5000;
+
+expressApp.use(Express.urlencoded({ extended: true }));
+expressApp.listen(port);
+
