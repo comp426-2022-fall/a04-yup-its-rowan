4e3b4b0473caf64f4c85bf53159f012db7fd8eee
Rohan Akki <roha.akki@gmail.com>
2022-12-01 19:08:28 -0500

    finishing more tests hopeully

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import {roll} from "./lib/roll.js";
 import minimist from "minimist";
-import { express } from "express";
+import { application, express } from "express";
 
 let args = minimist(process.argv.slice(2));
 let expressApp = express();
@@ -12,5 +12,9 @@ expressApp.listen(port);
 
 expressApp.get("/app", (req, res) => { //get request
   res.status(200).send("200 OK"); //send 200 OK for the first thing
-});
+    });
 
+expressApp.get("/app/roll", (req, res) => {
+    let result = roll(); 
+    res.status(200).send(JSON.stringify(result)); //send the result
+    });
